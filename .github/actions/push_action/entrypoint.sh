f9e4b3127e8539db992a2e72826059b73e17c977
Quoc Ho <phuquocvn@gmail.com>
2020-03-19 13:56:55 +0000

    Test dry run

--- .github/actions/push_action/entrypoint.sh
+++ .github/actions/push_action/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh -l
 set -x
 
-push() {
-    ACTOR=$1
-    TOKEN=$2
-    VERSION=$3
+initialize() {
+    VERSION=$1
     cd ~
-    # git clone --depth 1 https://github.com/TeX-for-Gmail/TeX-Live-Files.git texlivefiles
     mkdir texlivefiles
     cd texlivefiles
     git init
@@ -17,13 +14,21 @@ push() {
     git add .
     git commit -m "$VERSION"
     git tag -a $VERSION -m "$VERSION"
+}
+
+push() {
+    ACTOR=$1
+    TOKEN=$2
+    VERSION=$3
     git remote add origin https://${ACTOR}:${TOKEN}@github.com/TeX-for-Gmail/TeX-Live-Files.git
     git push origin master -f
     git push origin $VERSION
 }
 
+initialize $4
+
 if [ "$1" = "push" ]; then
     push $2 $3 $4
 else
-    echo "Push command is not issued!"
+    echo "Dry run completed. Push command is not issued!"
 fi
